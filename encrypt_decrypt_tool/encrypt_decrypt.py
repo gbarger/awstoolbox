8502eb9367badfd0763371bfb968ff56ac85508d
Glen Barger <gbarger@expedia.com>
2019-05-20 11:41:54 -0500

    added try except to encrypt and decrypt operations in menu only

--- encrypt_decrypt_tool/encrypt_decrypt.py
+++ encrypt_decrypt_tool/encrypt_decrypt.py
@@ -1,7 +1,8 @@
+import sys
 from os import system, name
 import base64
+import traceback
 import boto3
-
 
 def main():
     kms_client = get_kms_client()
@@ -20,7 +21,6 @@ def main():
             return None
         else:
             print("Please select a valid value.\n")
-
 
 def encrypt_menu(kms_client):
     clear()
@@ -41,13 +41,17 @@ def encrypt_menu(kms_client):
             input_key = input("provide input key or alias: ")
             input_string = input("string to encrypt: ")
 
-            print("\nencrypted string:\n{}\n".format(encrypt_value(kms_client, input_key, input_string)))
+            try:
+                encrypted_string = encrypt_value(kms_client, input_key, input_string)
+                print("\nencrypted string:\n{}\n".format(encrypted_string))
+            except:
+                exception_message = get_stack_trace()
+                print("error encrypting the string: {}".format(exception_message))
         elif input_value == "3":
             clear()
             return None
         else:
             print("Please select a valid value.\n")
-
 
 def encrypt_value(kms_client, input_key, input_string):
     ciphertext = kms_client.encrypt(
@@ -56,7 +60,6 @@ def encrypt_value(kms_client, input_key, input_string):
     )
 
     return  base64.b64encode(ciphertext["CiphertextBlob"]).decode("utf-8")
-
 
 def decrypt_menu(kms_client):
     clear()
@@ -68,13 +71,17 @@ def decrypt_menu(kms_client):
             clear()
             input_string = input("string to decrypt: ")
 
-            print("\ndecrypted string:\n{}\n".format(decrypt_value(kms_client, input_string)))
+            try:
+                decrypted_string = decrypt_value(kms_client, input_string)
+                print("\ndecrypted string:\n{}\n".format(decrypted_string))
+            except:
+                exception_message = get_stack_trace()
+                print("error decrypting the string: {}".format(exception_message))
         elif input_value == "2":
             clear()
             return None
         else:
             print("Please select a valid value.\n")
-
 
 def decrypt_value(kms_client, input_string):
     plaintext = kms_client.decrypt(
@@ -83,13 +90,11 @@ def decrypt_value(kms_client, input_string):
 
     return plaintext["Plaintext"].decode("utf-8")
 
-
 def get_kms_client():
     session = boto3.session.Session()
     kms_client = session.client('kms')
 
     return kms_client
-
 
 def clear():
     if name == 'nt': 
@@ -97,6 +102,19 @@ def clear():
     else: 
         system('clear')
 
+def get_stack_trace():
+    """This captures exception stack trace and format into proper dict which will be sent to error log."""
+    ex_type, ex_value, ex_traceback = sys.exc_info()
+    trace_back = traceback.extract_tb(ex_traceback)
+
+    # Format stacktrace
+    stack_trace = list()
+    stack_trace.append("Type: {}".format(ex_type))
+    stack_trace.append("Value: {}".format(ex_value))
+    for trace in trace_back:
+        stack_trace.append("File : %s , Line : %d, Func.Name : %s, Line : %s" % (trace[0], trace[1], trace[2], trace[3].replace("'", "")))
+
+    return stack_trace
 
 if __name__ == "__main__":
     main()
